624bf3abc081c18c56935183a2c45fb237ddbca0
YomnaYousry <yomnayousry19@gmail.com>
2023-04-24 07:03:59 -0700

    Task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int m;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			if (i <= m)
+			{
+				putchar(i + '0');
+				putchar(m + '0');
+
+				if (i == 9 && m == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
