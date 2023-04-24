4e3db23a32874e205cf6ae0f5ef4bd2d6728f64d
YomnaYousry <yomnayousry19@gmail.com>
2023-04-24 03:31:55 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if (lowercase != 'e' && lowercase != 'q')
+		{
+			putchar(lowercase);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
