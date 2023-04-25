c10404aea1b98c4a3377e6a794c81bee9c78172e
YomnaYousry <yomnayousry19@gmail.com>
2023-04-25 16:28:24 -0700

    Task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 
 /**
  *_islower - Used to check for lowercase characters;
+  *c: This is the input paratemer that we need to check if its lower case or not
+ *Return: Return 1 if the character is lower case and return zero if the charcter is not lower case
  */
 
 int _islower(int c)
