31d752b55405a630c9e86bceab570e095a3c33c9
YomnaYousry <yomnayousry19@gmail.com>
2023-04-25 17:36:26 -0700

    Task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add -  add two integers
+ *@a: 1st integer
+ *@b: 2nd integer
+ *Return: print sum of 2 integers
+ */
+
+int add(int a, int b)
+
+{
+	_putchar(a + b + '0');
+
+}
