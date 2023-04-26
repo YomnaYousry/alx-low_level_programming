144c4fd50e512585d6de81912777015134f435b7
YomnaYousry <yomnayousry19@gmail.com>
2023-04-25 17:15:01 -0700

    Task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_abs -  print last digit of number
+ *@c: number to get its last digit
+ *Return:print last digit
+ *and 0 if 0
+ */
+
+int print_last_digit(int c)
+
+{
+	char result;
+
+	if (c < 0)
+	{
+		c = c *-1;
+	}
+
+	result = c % 10;
+	return (result);
+
+}
