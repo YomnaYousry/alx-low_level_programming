50c899aabfcc573a4ee110f515e04e7ef3d6a39a
YomnaYousry <yomnayousry19@gmail.com>
2023-04-28 16:18:31 -0700

    Task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void print_alphabet(void);
 
-
-
-#endif
+#endif /* MAIN_H */
