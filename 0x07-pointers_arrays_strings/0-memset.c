882d20e41835569ee695f814651bd43d14c1487c
YomnaYousry <yomnayousry19@gmail.com>
2023-05-08 14:16:17 -0700

    Task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *@n: Number of Bytes
+ *@b: Fixed byte
+ *@s:Pointer
+ *Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = b;
+		return s;
+	}
+}
